9dbeb105349d65622a298d7ed67e85d31059b5c7
Sam Oen <samoen@gmail.com>
2024-03-06 17:39:15 +0800

    initial

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-web';
+
+const mylist = [{title:'oh'}, {title:'yes'}, {title:'cool'}]
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
+      <FlatList
+        data={
+          mylist
+        }
+        renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +27,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item:{
+    backgroundColor:'pink',
+    marginTop:'5px'
+  }
 });
