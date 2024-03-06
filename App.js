cbbbdc24adc3e1a0c8e0d0ebe0a9e0a76679d646
Sam Oen <samoen@gmail.com>
2024-03-06 19:28:06 +0800

    fetch issues

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { FlatList } from 'react-native-web';
+// import { Octokit } from "octokit";
 
+const githubToken = process.env.EXPO_PUBLIC_GITHUB_PERSONAL_TOKEN;
+// const octokit = new Octokit({ 
+  //   auth: githubToken
+  // });
 const mylist = [{title:'oh'}, {title:'yes'}, {title:'cool'}]
+let myState = 'oooh lala'
+async function getIssues(){
+  try{
+      const api = await fetch(
+        `https://api.github.com/issues`,
+        {
+          headers: new Headers({
+            'Authorization':`Bearer ${githubToken}`,
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          })
+        }
+      ) 
+        
+      const data = await api.json();
+      console.log(data);
+      return data;
+
+  }catch(error){
+      console.log(`fetch errored ${error}`);
+  }
+}
+const d = getIssues();
+// console.log(d)
+
 
 export default function App() {
   
+  console.log('hi');
+  console.log('token is ' + githubToken)
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={
-          mylist
-        }
+        data={mylist}
         renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
       />
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>mystate {myState}</Text>
       <StatusBar style="auto" />
     </View>
   );
